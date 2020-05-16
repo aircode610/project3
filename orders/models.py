5493cfbbb7d346d23dc19a6601bf190fcad9d56f
Amirali <aircode610@gmail.com>
2020-05-16 19:08:30 +0430

    Added the signin/login without styling

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=60)
+    lastname = models.CharField(max_length=60)
+    email = models.CharField(max_length=60)
+    username = models.CharField(max_length=60)
+    password = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f"{self.name} {self.lastname} ({self.email} : {self.username} - {self.password})"
--- pizza/settings.py
+++ pizza/settings.py
@@ -119,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST_USER = "aircode610@gmail.com"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = "@Aircode610"
