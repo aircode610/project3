0969b381ae2c178dba6fb38a6054934379e0406b
Amirali <aircode610@gmail.com>
2020-05-17 14:57:46 +0430

    Added the menu

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,88 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=60)
+    lastname = models.CharField(max_length=60)
+    email = models.CharField(max_length=60)
+    username = models.CharField(max_length=60)
+    password = models.CharField(max_length=60)
+
+    def __str__(self):
+        return f"{self.name} {self.lastname} ({self.email} : {self.username} - {self.password})"
+
+
+class Pizza(models.Model):
+    NAME = (
+        ('Regular', 'Regular'),
+        ('Sicilian', 'Sicilian'),
+    )
+    KIND = (
+        ('Cheese', 'Cheese'),
+        ('1 topping', '1 topping'),
+        ('2 toppings', '2 toppings'),
+        ('3 toppings', '3 toppings'),
+        ('Special', 'Special'),
+    )
+    SIZE = (
+        ('Small', 'Small'),
+        ('Large', 'Large'),
+    )
+    name = models.CharField(max_length=10, choices=NAME)
+    kind = models.CharField(max_length=10, choices=KIND)
+    size = models.CharField(max_length=10, choices=SIZE)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} - {self.kind} ( {self.size} ) : {self.price}"
+
+
+class Topping(models.Model):
+    name = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.name}"
+
+
+class Sub(models.Model):
+    SIZE = (
+        ('Small', 'Small'),
+        ('Large', 'Large'),
+    )
+
+    name = models.CharField(max_length=20)
+    size = models.CharField(max_length=10, choices=SIZE)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} ({self.size}) : {self.price}"
+
+
+class Pasta(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} : {self.price}"
+
+
+class Salad(models.Model):
+    name = models.CharField(max_length=40)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} : {self.price}"
+
+
+class Dinner_Platter(models.Model):
+    SIZE = (
+        ('Small', 'Small'),
+        ('Large', 'Large'),
+    )
+
+    name = models.CharField(max_length=20)
+    size = models.CharField(max_length=10, choices=SIZE)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} ({self.size}) : {self.price}"
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,152 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render, reverse
+from django.core.mail import send_mail
+
+from .models import User, Pizza, Topping, Sub, Pasta, Salad, Dinner_Platter
+
+import random
+import json
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    try:
+        if request.session["logged_in"] == False:
+            return HttpResponseRedirect(reverse("signin"))
+        else:
+
+            pizzas = []
+            for pizza in Pizza.objects.all():
+                pizzas.append({
+                    "name" : pizza.name,
+                    "kind" : pizza.kind,
+                    "size" : pizza.size,
+                    "price" : pizza.price
+                })
+
+            toppings = []
+            for topping in Topping.objects.all():
+                toppings.append({
+                    "name" : topping.name,
+                })
+
+            subs = []
+            for sub in Sub.objects.all():
+                subs.append({
+                    "name" : sub.name,
+                    "size" : sub.size,
+                    "price" : sub.price
+                })
+
+            pastas = []
+            for pasta in Pasta.objects.all():
+                pastas.append({
+                    "name" : pasta.name,
+                    "price" : pasta.price
+                })
+
+            salads = []
+            for salad in Salad.objects.all():
+                salads.append({
+                    "name" : salad.name,
+                    "price" : salad.price
+                })
+
+            dinners = []
+            for dinner in Dinner_Platter.objects.all():
+                dinners.append({
+                    "name" : dinner.name,
+                    "size" : dinner.size,
+                    "price" : dinner.price
+                })
+
+            food = { "Pizza" : pizzas, "Topping" : toppings, "Sub" : subs,
+             "Pasta" :pastas, "Salad" : salads, "Dinner platter" : dinners }
+
+            context = {
+                "username" : request.session["username"],
+                "food" : json.dumps(food)
+            }
+            return render(request, "orders/index.html", context)
+
+    except:
+        request.session["logged_in"] = False
+        return HttpResponseRedirect(reverse("signin"))
+
+
+def signin(request):
+    context = {
+        "users" : User.objects.all()
+    }
+
+    return render(request, "orders/signin.html", context)
+
+def verify(request):
+    email = request.POST["email"]
+    code = random.randint(1000, 9999)
+    content = f"Your code: {code}"
+
+    name = request.POST["name"]
+    lastname = request.POST["lastname"]
+    username = request.POST["username"]
+    password = request.POST["password"]
+
+    request.session["user_info"] = { "name" : name, "lastname" : lastname, "email" : email,
+                                    "username" : username, "password" : password }
+
+    send_mail(
+        'Email verification',
+        content,
+        'aircode610@gmail.com',
+        [email],
+        fail_silently=False
+    )
+
+    context = {
+        "code" : code
+    }
+
+    return render(request, "orders/verify.html", context)
+
+
+def add_user(request):
+    info = request.session["user_info"]
+
+    new_user = User(name=info["name"], lastname=info["lastname"], email=info["email"],
+                    username=info["username"], password=info["password"])
+    new_user.save()
+
+    request.session["logged_in"] = True
+
+    return HttpResponseRedirect(reverse("index"))
+
+
+def login(request):
+    context = {
+        "users" : User.objects.all()
+    }
+
+    return render(request, "orders/login.html", context)
+
+
+def login_user(request):
+    request.session["logged_in"] = True
+
+    users =  User.objects.all()
+
+    for user in users:
+        if user.username == request.POST["username"]:
+
+            request.session["user_info"] = { "name" : user.name, "lastname" : user.lastname,
+             "email" : user.email, "username" : user.username, "password" : user.password }
+
+            request.session["username"] = user.username
+
+            break
+
+    return HttpResponseRedirect(reverse("index"))
+
+
+def logout(request):
+    request.session["logged_in"] = False
+
+    return HttpResponseRedirect(reverse("index"))
