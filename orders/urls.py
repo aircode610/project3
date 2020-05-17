9b9057d8177ac210f4919d4c68ae8bad70e3e02a
Amirali <aircode610@gmail.com>
2020-05-17 14:57:46 +0430

    Added the order part

--- orders/urls.py
+++ orders/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("signin", views.signin, name="signin"),
+    path("verify", views.verify, name="verify"),
+    path("add_user", views.add_user, name="add_user"),
+    path("login", views.login, name="login"),
+    path("login_user", views.login_user, name="login_user"),
+    path("logout", views.logout, name="logout"),
+    path("order", views.order, name="order")
 ]
